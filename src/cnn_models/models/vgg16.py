e9420ae92f4ade9c4ed5241744e72c2ff8ffab58
filip141 <filip141@gmail.com>
2017-10-18 02:03:56 +0200

    not work

--- src/cnn_models/models/vgg16.py
+++ src/cnn_models/models/vgg16.py
@@ -1,4 +1,4 @@
-from simple_network.models import NetworkModel
+from simple_network.models import NetworkParallel
 from simple_network.layers import ConvolutionalLayer, MaxPoolingLayer, ReluLayer, FullyConnectedLayer, \
      Flatten, DropoutLayer, BatchNormalizationLayer
 
@@ -11,14 +11,13 @@ class VGG16Model(object):
         self.output_size = output_size
 
         # Define model
-        self.net_model = NetworkModel(input_size, metric=["accuracy", "cross_entropy"],
-                                      input_summary=self.input_summary, summary_path=log_path)
+        self.net_model = NetworkParallel(input_size, metric=["accuracy", "cross_entropy"],
+                                         input_summary=self.input_summary, summary_path=log_path)
 
     def build_model(self):
         self.net_model.add(ConvolutionalLayer([3, 3, 64], initializer="xavier", name='convo_layer_1_1'))
         self.net_model.add(BatchNormalizationLayer(name="batch_normalization_1_1"))
         self.net_model.add(ReluLayer(name="relu_1_1"))
-        self.net_model.add(DropoutLayer(percent=0.3))
 
         self.net_model.add(ConvolutionalLayer([3, 3, 64], initializer="xavier", name='convo_layer_1_2'))
         self.net_model.add(BatchNormalizationLayer(name="batch_normalization_1_2"))
@@ -28,7 +27,6 @@ class VGG16Model(object):
         self.net_model.add(ConvolutionalLayer([3, 3, 128], initializer="xavier", name='convo_layer_2_1'))
         self.net_model.add(BatchNormalizationLayer(name="batch_normalization_2_1"))
         self.net_model.add(ReluLayer(name="relu_2_1"))
-        self.net_model.add(DropoutLayer(percent=0.4))
 
         self.net_model.add(ConvolutionalLayer([3, 3, 128], initializer="xavier", name='convo_layer_2_2'))
         self.net_model.add(BatchNormalizationLayer(name="batch_normalization_2_2"))
@@ -38,12 +36,10 @@ class VGG16Model(object):
         self.net_model.add(ConvolutionalLayer([3, 3, 256], initializer="xavier", name='convo_layer_3_1'))
         self.net_model.add(BatchNormalizationLayer(name="batch_normalization_3_1"))
         self.net_model.add(ReluLayer(name="relu_3_1"))
-        self.net_model.add(DropoutLayer(percent=0.4))
 
         self.net_model.add(ConvolutionalLayer([3, 3, 256], initializer="xavier", name='convo_layer_3_2'))
         self.net_model.add(BatchNormalizationLayer(name="batch_normalization_3_2"))
         self.net_model.add(ReluLayer(name="relu_3_2"))
-        self.net_model.add(DropoutLayer(percent=0.4))
 
         self.net_model.add(ConvolutionalLayer([3, 3, 256], initializer="xavier", name='convo_layer_3_3'))
         self.net_model.add(BatchNormalizationLayer(name="batch_normalization_3_3"))
@@ -53,12 +49,10 @@ class VGG16Model(object):
         self.net_model.add(ConvolutionalLayer([3, 3, 512], initializer="xavier", name='convo_layer_4_1'))
         self.net_model.add(BatchNormalizationLayer(name="batch_normalization_4_1"))
         self.net_model.add(ReluLayer(name="relu_4_1"))
-        self.net_model.add(DropoutLayer(percent=0.4))
 
         self.net_model.add(ConvolutionalLayer([3, 3, 512], initializer="xavier", name='convo_layer_4_2'))
         self.net_model.add(BatchNormalizationLayer(name="batch_normalization_4_2"))
         self.net_model.add(ReluLayer(name="relu_4_2"))
-        self.net_model.add(DropoutLayer(percent=0.4))
 
         self.net_model.add(ConvolutionalLayer([3, 3, 512], initializer="xavier", name='convo_layer_4_3'))
         self.net_model.add(BatchNormalizationLayer(name="batch_normalization_4_3"))
@@ -68,19 +62,16 @@ class VGG16Model(object):
         self.net_model.add(ConvolutionalLayer([3, 3, 512], initializer="xavier", name='convo_layer_5_1'))
         self.net_model.add(BatchNormalizationLayer(name="batch_normalization_5_1"))
         self.net_model.add(ReluLayer(name="relu_5_1"))
-        self.net_model.add(DropoutLayer(percent=0.4))
 
         self.net_model.add(ConvolutionalLayer([3, 3, 512], initializer="xavier", name='convo_layer_5_2'))
         self.net_model.add(BatchNormalizationLayer(name="batch_normalization_5_2"))
         self.net_model.add(ReluLayer(name="relu_5_2"))
-        self.net_model.add(DropoutLayer(percent=0.4))
 
         self.net_model.add(ConvolutionalLayer([3, 3, 512], initializer="xavier", name='convo_layer_5_3'))
         self.net_model.add(BatchNormalizationLayer(name="batch_normalization_5_3"))
         self.net_model.add(ReluLayer(name="relu_5_3"))
         self.net_model.add(MaxPoolingLayer(pool_size=[2, 2], stride=2, padding="valid", name="pooling_5_3"))
 
-        self.net_model.add(DropoutLayer(percent=0.5))
         self.net_model.add(Flatten(name='flatten_6'))
 
         self.net_model.add(FullyConnectedLayer([512 * (self.input_size[0] / 32.0)**2, 512],
@@ -91,36 +82,38 @@ class VGG16Model(object):
 
         self.net_model.add(FullyConnectedLayer([512, self.output_size], initializer="xavier",
                                                name='fully_connected_7_1'))
-        self.net_model.set_optimizer("Adam", beta_1=0.9, beta_2=0.999, epsilon=1e-08)
+        # self.net_model.set_optimizer("Adam", beta_1=0.9, beta_2=0.999, epsilon=1e-08)
+        self.net_model.set_optimizer("SGD")
         self.net_model.set_loss("cross_entropy")
 
-    def train(self, train_iterator, test_iterator, learning_rate, batch_size, restore_model=False, epochs=300):
+    def train(self, train_iterator, test_iterator, learning_rate, batch_size, restore_model=False, epochs=300,
+              embedding_num=None):
         self.net_model.build_model(learning_rate)
         if restore_model:
             self.net_model.restore()
         self.net_model.train(train_iter=train_iterator, train_step=batch_size, test_iter=test_iterator,
-                             test_step=batch_size, sample_per_epoch=391, epochs=epochs)
+                             test_step=batch_size, sample_per_epoch=391, epochs=epochs, embedding_num=embedding_num)
 
 if __name__ == '__main__':
     # from cnn_models.iterators.imagenet import DogsDataset
-    # train_path = '/home/phoenix/Datasets/StanfordDogs/Images'
-    # labels_path = '/home/phoenix/Datasets/StanfordDogs/Annotation'
-    # class_names_path = '/home/phoenix/Datasets/StanfordDogs/class_names.txt'
+    # train_path = '/home/filip/Datasets/StanfordDogs/Images'
+    # labels_path = '/home/filip/Datasets/StanfordDogs/Annotation'
+    # class_names_path = '/home/filip/Datasets/StanfordDogs/class_names.txt'
     # im_net_train = DogsDataset(data_path=train_path, labels_path=labels_path, class_names=class_names_path,
     #                            train_set=True)
     # im_net_test = DogsDataset(data_path=train_path, labels_path=labels_path, class_names=class_names_path,
     #                           train_set=False)
-    # im_net_model = VGG16Model(input_size=[64, 64, 3], output_size=120, log_path="/home/phoenix/tensor_logs")
+    # im_net_model = VGG16Model(input_size=[64, 64, 3], output_size=120, log_path="/home/filip/tensor_logs")
     # im_net_model.build_model()
-    # im_net_model.train(im_net_train, im_net_test, 0.001, 64, epochs=300)
+    # im_net_model.train(im_net_train, im_net_test, 0.0005, 64, epochs=300)
     from cnn_models.iterators.cifar import CIFARDataset
     train_path = "/home/filip/Datasets/cifar/train"
     test_path = "/home/filip/Datasets/cifar/test"
-    cifar_train = CIFARDataset(data_path=train_path, resolution="64x64")
-    cifar_test = CIFARDataset(data_path=test_path, resolution="64x64")
+    cifar_train = CIFARDataset(data_path=train_path, resolution="32x32")
+    cifar_test = CIFARDataset(data_path=test_path, resolution="32x32")
 
-    im_net_model = VGG16Model(input_size=[64, 64, 3], output_size=10, log_path="/home/filip/tensor_logs")
+    im_net_model = VGG16Model(input_size=[32, 32, 3], output_size=10, log_path="/home/filip/tensor_logs")
     im_net_model.build_model()
-    im_net_model.train(cifar_train, cifar_test, 0.0001, 32, epochs=300)
+    im_net_model.train(cifar_train, cifar_train, 0.005, 32, epochs=300, embedding_num=512)
 
 
