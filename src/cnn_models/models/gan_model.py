843823ac8798c713fd4d92f2dcd7fd2f96ab45bf
filip <filip141@gmail.com>
2017-11-07 01:53:31 +0100

    few changes to gan

--- src/cnn_models/models/gan_model.py
+++ src/cnn_models/models/gan_model.py
@@ -2,7 +2,7 @@ import logging
 import numpy as np
 from simple_network.models import GANScheme
 from simple_network.layers import DeconvolutionLayer, FullyConnectedLayer, ConvolutionalLayer, ReshapeLayer, \
-    GlobalAveragePoolingLayer, LeakyReluLayer, BatchNormalizationLayer, ReluLayer
+    LeakyReluLayer, BatchNormalizationLayer, DropoutLayer, Flatten, SigmoidLayer, LinearLayer
 
 
 logging.basicConfig(level=logging.INFO)
@@ -11,59 +11,54 @@ logger = logging.getLogger(__name__)
 
 class GANNetwork(GANScheme):
 
-    def __init__(self, generator_input_size, discriminator_input_size, log_path):
+    def __init__(self, generator_input_size, discriminator_input_size, log_path, batch_size=None):
         super(GANNetwork, self).__init__(generator_input_size, discriminator_input_size, log_path)
+        self.batch_size = batch_size
 
     def build_generator(self, generator):
-        generator.add(FullyConnectedLayer([np.prod(self.generator_input_size), 4096], initializer="xavier",
-                                          name='fully_connected_1'))
-        generator.add(ReluLayer(name="relu_1"))
-        generator.add(ReshapeLayer(output_shape=[2, 2, 1024]))
-        generator.add(DeconvolutionLayer([5, 5, 512], output_shape=[4, 4, 512], initializer="xavier",
-                                         name='deconv_layer_2', stride=2))
-        # generator.add(BatchNormalizationLayer(name="batch_normalization_2"))
-        generator.add(ReluLayer(name="relu_2"))
-        generator.add(DeconvolutionLayer([5, 5, 256], output_shape=[8, 8, 256], initializer="xavier",
-                                         name='deconv_layer_3', stride=2))
-        # generator.add(BatchNormalizationLayer(name="batch_normalization_3"))
-        generator.add(ReluLayer(name="relu_3"))
-        generator.add(DeconvolutionLayer([5, 5, 128], output_shape=[16, 16, 128], initializer="xavier",
-                                         name='deconv_layer_4', stride=2))
-        # generator.add(BatchNormalizationLayer(name="batch_normalization_4"))
-        generator.add(ReluLayer(name="relu_4"))
-        generator.add(DeconvolutionLayer([5, 5, 64], output_shape=[32, 32, 64], initializer="xavier",
-                                         name='deconv_layer_5', stride=2))
-        # generator.add(BatchNormalizationLayer(name="batch_normalization_5"))
-        generator.add(ReluLayer(name="relu_5"))
-        generator.add(DeconvolutionLayer([3, 3, 1], output_shape=[32, 32, 1], initializer="xavier",
-                                         name='deconv_layer_6', stride=1))
+        generator.add(FullyConnectedLayer([np.prod(self.generator_input_size), 12544], initializer="xavier",
+                                          name='fully_connected_g_1'))
+        generator.add(LeakyReluLayer(alpha=0.2, name="leaky_relu_g_1"))
+        generator.add(ReshapeLayer(output_shape=[7, 7, 256], name="reshape_g_1"))
+
+        # Deconvolution 1
+        generator.add(DeconvolutionLayer([4, 4, 64], output_shape=[14, 14, 64], initializer="xavier",
+                                         name='deconv_layer_g_2', stride=2, batch_size=self.batch_size))
+        generator.add(BatchNormalizationLayer(name="batch_normalization_g_2"))
+        generator.add(DropoutLayer(percent=0.4, name="dropout_g_2"))
+        generator.add(LeakyReluLayer(alpha=0.2, name="leaky_relu_g_2"))
+
+        # Deconvolution 2
+        generator.add(DeconvolutionLayer([4, 4, 1], output_shape=[28, 28, 1], initializer="xavier",
+                                         name='deconv_layer_g_3', stride=2, batch_size=self.batch_size))
+        generator.add(SigmoidLayer(name="sigmoid_g_3"))
 
     def build_discriminator(self, discriminator):
-        discriminator.add(ConvolutionalLayer([5, 5, 128], initializer="xavier", name='convo_layer_1', stride=2))
-        # discriminator.add(BatchNormalizationLayer(name="batch_normalization_d_1"))
-        discriminator.add(LeakyReluLayer(alpha=0.1))
-        discriminator.add(ConvolutionalLayer([5, 5, 256], initializer="xavier", name='convo_layer_2', stride=2))
-        # discriminator.add(BatchNormalizationLayer(name="batch_normalization_d_2"))
-        discriminator.add(LeakyReluLayer(alpha=0.1))
-        discriminator.add(ConvolutionalLayer([5, 5, 512], initializer="xavier", name='convo_layer_3', stride=2))
-        # discriminator.add(BatchNormalizationLayer(name="batch_normalization_d_3"))
-        discriminator.add(LeakyReluLayer(alpha=0.1))
-        discriminator.add(ConvolutionalLayer([5, 5, 1024], initializer="xavier", name='convo_layer_4', stride=2))
-        # discriminator.add(BatchNormalizationLayer(name="batch_normalization_d_4"))
-        discriminator.add(LeakyReluLayer(alpha=0.1))
-        discriminator.add(GlobalAveragePoolingLayer())
-        discriminator.add(FullyConnectedLayer([1024, 1], initializer="xavier", name='fully_connected_5'))
+        # Convolutional 1
+        discriminator.add(ConvolutionalLayer([4, 4, 64], initializer="xavier", name='convo_layer_d_1', stride=2))
+        discriminator.add(BatchNormalizationLayer(name="batch_normalization_d_1"))
+        discriminator.add(DropoutLayer(percent=0.4, name="dropout_d_1"))
+        discriminator.add(LeakyReluLayer(alpha=0.2, name="leaky_relu_d_1"))
 
+        # Convolutional 2
+        discriminator.add(ConvolutionalLayer([4, 4, 128], initializer="xavier", name='convo_layer_d_2', stride=2))
+        discriminator.add(BatchNormalizationLayer(name="batch_normalization_d_2"))
+        discriminator.add(DropoutLayer(percent=0.4, name="dropout_d_2"))
+        discriminator.add(LeakyReluLayer(alpha=0.2, name="leaky_relu_d_2"))
+
+        # Dense 1
+        discriminator.add(Flatten(name="flatten_d"))
+        discriminator.add(FullyConnectedLayer([6272, 256], initializer="xavier", name='fully_connected_d_3'))
+        discriminator.add(LeakyReluLayer(alpha=0.2, name="leaky_relu_d_3"))
+
+        # Dense 2
+        discriminator.add(FullyConnectedLayer([256, 1], initializer="xavier", name='fully_connected_d_4'))
+        discriminator.add(LinearLayer(name="linear_d_4"))
 
 if __name__ == '__main__':
     from cnn_models.iterators.mnist import MNISTDataset
-    mnist = MNISTDataset("/home/filip/Datasets", resolution="32x32", label_only=3)
-    # from cnn_models.iterators.cifar import CIFARDataset
-    # train_path = "/home/filip/Datasets/cifar/train"
-    # test_path = "/home/filip/Datasets/cifar/test"
-    # cifar_train = CIFARDataset(data_path=train_path)
-    # cifar_test = CIFARDataset(data_path=test_path)
-    gan = GANNetwork(generator_input_size=[100, ], discriminator_input_size=[32, 32, 1],
-                     log_path="/home/filip/tensor_logs")
-    gan.build_model(0.0008, 0.0004)
-    gan.train(mnist, 1, 64, epochs=300, sample_per_epoch=1875)
+    mnist = MNISTDataset("/home/filip/Datasets", resolution="28x28")
+    gan = GANNetwork(generator_input_size=[100, ], discriminator_input_size=[28, 28, 1],
+                     log_path="/home/filip/tensor_logs", batch_size=128)
+    gan.build_model(0.0001, 0.0001)
+    gan.train(mnist, 1, 128, epochs=300, sample_per_epoch=390)
