5253db6d04d4ed2493b25bb45f0c8a6aeace96fe
filip141 <filip141@gmail.com>
2018-01-19 22:47:53 +0100

    changed dogs dataset

--- src/cnn_models/iterators/imagenet.py
+++ src/cnn_models/iterators/imagenet.py
@@ -8,8 +8,7 @@ from xml.dom import minidom
 class DogsDataset(object):
 
     def __init__(self, data_path="./Images", labels_path="./Annotation", class_names="./class_names.txt",
-                 resize_img="64x64", train_set=True, force_overfit=False, normalize_to_one=False):
-        self.normalize_to_one = normalize_to_one
+                 resize_img="64x64", train_set=True, force_overfit=False):
         self.data_path = data_path
         self.labels_path = labels_path
         self.class_names = class_names
@@ -50,7 +49,7 @@ class DogsDataset(object):
         batch_labels = np.zeros((number, len(self.class_dict)))
         batch_matrix = np.zeros((number, self.res_tuple[0], self.res_tuple[1], 3))
         first_one_hot_labels[self.class_dict[first_label]] = 1.0
-        batch_matrix[0] = (first_img_res.astype('float32') - np.mean(first_img_res)) / np.std(first_img_res)
+        batch_matrix[0] = first_img_res / 255.
         batch_labels[0] = first_one_hot_labels
         self.position += 1
         self.position = self.position if self.position < len(self.file_list) else 0
@@ -78,10 +77,7 @@ class DogsDataset(object):
 
             one_hot_labels = np.zeros((len(self.class_dict),))
             one_hot_labels[self.class_dict[img_label]] = 1.0
-            if self.normalize_to_one:
-                batch_matrix[img_idx] = img_res.astype('float32') / np.max(img_res)
-            else:
-                batch_matrix[img_idx] = (img_res.astype('float32') - np.mean(img_res)) / np.std(img_res)
+            batch_matrix[img_idx] = img_res / 255.
             batch_labels[img_idx] = one_hot_labels
             self.position += 1
             self.position = self.position if self.position < len(self.file_list) else 0
--- src/cnn_models/models/gan_models/gan_dogs.py
+++ src/cnn_models/models/gan_models/gan_dogs.py
@@ -1,8 +1,9 @@
 import logging
 import numpy as np
-from simple_network.models import GANScheme
+import tensorflow as tf
+from simple_network.models import MMGANScheme
 from simple_network.layers import DeconvolutionLayer, FullyConnectedLayer, ConvolutionalLayer, ReshapeLayer, \
-    LeakyReluLayer, BatchNormalizationLayer, DropoutLayer, Flatten, SigmoidLayer, \
+    LeakyReluLayer, SingleBatchNormLayer, DropoutLayer, Flatten, SigmoidLayer, \
     LinearLayer, ReluLayer, GlobalAveragePoolingLayer, TanhLayer
 
 
@@ -10,72 +11,82 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-class GANNetwork(GANScheme):
+class GANNetwork(MMGANScheme):
 
-    def __init__(self, generator_input_size, discriminator_input_size, log_path, batch_size=None):
-        super(GANNetwork, self).__init__(generator_input_size, discriminator_input_size, log_path)
+    def __init__(self, generator_input_size, discriminator_input_size, log_path, batch_size=None, labels=None,
+                 labels_size=10):
+        super(GANNetwork, self).__init__(generator_input_size, discriminator_input_size, log_path, batch_size,
+                                         labels=labels, labels_size=labels_size)
         self.batch_size = batch_size
 
     def build_generator(self, generator):
-        generator.add(FullyConnectedLayer([np.prod(self.generator_input_size), 4096], initializer="xavier",
+        generator.add(FullyConnectedLayer(out_neurons=4096, initializer="xavier",
                                           name='fully_connected_g_1'))
         generator.add(LeakyReluLayer(name="relu_1"))
         generator.add(ReshapeLayer(output_shape=[2, 2, 1024]))
         generator.add(DeconvolutionLayer([5, 5, 512], output_shape=[4, 4, 512], initializer="xavier",
-                                         name='deconv_layer_g_2', stride=2))
-        generator.add(BatchNormalizationLayer(name="batch_normalization_g_2"))
+                                         name='deconv_layer_g_2', stride=2, batch_size=self.batch_size))
+        generator.add(SingleBatchNormLayer(name="batch_normalization_g_2"))
         generator.add(LeakyReluLayer(name="relu_2"))
 
         generator.add(DeconvolutionLayer([5, 5, 256], output_shape=[8, 8, 256], initializer="xavier",
-                                         name='deconv_layer_g_3', stride=2))
-        generator.add(BatchNormalizationLayer(name="batch_normalization_g_3"))
+                                         name='deconv_layer_g_3', stride=2, batch_size=self.batch_size))
+        generator.add(SingleBatchNormLayer(name="batch_normalization_g_3"))
         generator.add(LeakyReluLayer(name="relu_3"))
 
         generator.add(DeconvolutionLayer([5, 5, 128], output_shape=[16, 16, 128], initializer="xavier",
-                                         name='deconv_layer_g_4', stride=2))
-        generator.add(BatchNormalizationLayer(name="batch_normalization_g_4"))
+                                         name='deconv_layer_g_4', stride=2, batch_size=self.batch_size))
+        generator.add(SingleBatchNormLayer(name="batch_normalization_g_4"))
         generator.add(LeakyReluLayer(name="relu_4"))
 
         generator.add(DeconvolutionLayer([5, 5, 64], output_shape=[32, 32, 64], initializer="xavier",
-                                         name='deconv_layer_g_5', stride=2))
-        generator.add(BatchNormalizationLayer(name="batch_normalization_g_5"))
+                                         name='deconv_layer_g_5', stride=2, batch_size=self.batch_size))
+        generator.add(SingleBatchNormLayer(name="batch_normalization_g_5"))
         generator.add(LeakyReluLayer(name="relu_5"))
-        generator.add(DeconvolutionLayer([3, 3, 3], output_shape=[32, 32, 3], initializer="xavier",
-                                         name='deconv_layer_g_6', stride=1))
-        generator.add(TanhLayer(name="tanh_g_5"))
+
+        generator.add(DeconvolutionLayer([5, 5, 32], output_shape=[64, 64, 32], initializer="xavier",
+                                         name='deconv_layer_g_6', stride=2, batch_size=self.batch_size))
+        generator.add(SingleBatchNormLayer(name="batch_normalization_g_6"))
+        generator.add(LeakyReluLayer(name="relu_6"))
+
+        generator.add(DeconvolutionLayer([3, 3, 3], output_shape=[64, 64, 3], initializer="xavier",
+                                         name='deconv_layer_g_7', stride=1, batch_size=self.batch_size))
+        generator.add(TanhLayer(name="tanh_g_7"))
 
     def build_discriminator(self, discriminator):
         discriminator.add(ConvolutionalLayer([5, 5, 128], initializer="xavier", name='convo_layer_d_1', stride=2))
-        discriminator.add(BatchNormalizationLayer(name="batch_normalization_d_1"))
+        discriminator.add(SingleBatchNormLayer(name="batch_normalization_d_1"))
         discriminator.add(LeakyReluLayer(alpha=0.1, name="leaky_relu_d_1"))
 
         discriminator.add(ConvolutionalLayer([5, 5, 256], initializer="xavier", name='convo_layer_d_2', stride=2))
-        discriminator.add(BatchNormalizationLayer(name="batch_normalization_d_2"))
+        discriminator.add(SingleBatchNormLayer(name="batch_normalization_d_2"))
         discriminator.add(LeakyReluLayer(alpha=0.1, name="leaky_relu_d_2"))
 
         discriminator.add(ConvolutionalLayer([5, 5, 512], initializer="xavier", name='convo_layer_d_3', stride=2))
-        discriminator.add(BatchNormalizationLayer(name="batch_normalization_d_3"))
+        discriminator.add(SingleBatchNormLayer(name="batch_normalization_d_3"))
         discriminator.add(LeakyReluLayer(alpha=0.1, name="leaky_relu_d_3"))
 
         discriminator.add(ConvolutionalLayer([5, 5, 1024], initializer="xavier", name='convo_layer_d_4', stride=2))
-        discriminator.add(BatchNormalizationLayer(name="batch_normalization_d_4"))
+        discriminator.add(SingleBatchNormLayer(name="batch_normalization_d_4"))
         discriminator.add(LeakyReluLayer(alpha=0.1, name="leaky_relu_d_4"))
 
         discriminator.add(GlobalAveragePoolingLayer())
-        discriminator.add(FullyConnectedLayer([1024, 1], initializer="xavier", name='fully_connected_5'))
+        discriminator.add(FullyConnectedLayer(out_neurons=1, initializer="xavier", name='fully_connected_5'))
         discriminator.add(LinearLayer(name="linear_d_5"))
 
 
 if __name__ == '__main__':
     from cnn_models.iterators.imagenet import DogsDataset
-    train_path = '/home/phoenix/Datasets/StanfordDogs/Images'
-    labels_path = '/home/phoenix/Datasets/StanfordDogs/Annotation'
-    class_names_path = '/home/phoenix/Datasets/StanfordDogs/class_names.txt'
+    train_path = '/home/filip/Datasets/StanfordDogs/Images'
+    labels_path = '/home/filip/Datasets/StanfordDogs/Annotation'
+    class_names_path = '/home/filip/Datasets/StanfordDogs/class_names.txt'
     im_net_train = DogsDataset(data_path=train_path, labels_path=labels_path, class_names=class_names_path,
-                               train_set=True, resize_img="32x32", normalize_to_one=True)
-    gan = GANNetwork(generator_input_size=[100, ], discriminator_input_size=[32, 32, 3],
-                     log_path="/home/phoenix/tensor_logs/GAN_DOGS", batch_size=128)
+                               train_set=True, resize_img="64x64")
+    gan = GANNetwork(generator_input_size=[100, ], discriminator_input_size=[64, 64, 3],
+                     log_path="/home/filip/tensor_logs/GAN_DOGS", batch_size=64, labels='convo-semi-supervised',
+                     labels_size=120)
     gan.set_discriminator_optimizer("Adam", beta_1=0.5)
     gan.set_generator_optimizer("Adam", beta_1=0.5)
-    gan.model_compile(generator_learning_rate=0.0008, discriminator_learning_rate=0.0001)
-    gan.train(im_net_train, discriminator_steps=10, train_step=128, epochs=300, sample_per_epoch=390)
+    gan.set_loss("js-non-saturation", label_smooth=True)
+    gan.model_compile(generator_learning_rate=0.0004, discriminator_learning_rate=0.0002)
+    gan.train(im_net_train, train_step=64, epochs=300, sample_per_epoch=1000, restore_model=False)
